58e31cf905885398e105cf40e07835a2e2d3d855
renanwilson <renan.alves.ra16@gmail.com>
2023-03-03 19:36:06 -0300

    feat: uptade app to routes and add providers

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,20 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { RoutesApp } from "routes/Routes";
+import "index.css";
+import { ListContextProvider } from "contexts/ListContext";
+import { SearchContextProvider } from "contexts/SearchContext";
+import { PageContextProvider } from "contexts/PageContext";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <ListContextProvider>
+        <SearchContextProvider>
+          <PageContextProvider>
+            <RoutesApp />
+          </PageContextProvider>
+        </SearchContextProvider>
+      </ListContextProvider>
     </div>
   );
 }
